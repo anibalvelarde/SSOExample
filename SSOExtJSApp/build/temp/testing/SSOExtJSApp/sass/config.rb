c0cabaea7f89a42ab46872939b00ca332cfe26d1
asunilkutty <sayyappa@NUSDLMJMYECR.aonnet.aon.net>
2015-10-23 15:12:15 -0500

    Built version of EXT JS Sample Applicationm

---
+++ SSOExtJSApp/build/temp/testing/SSOExtJSApp/sass/config.rb
@@ -0,0 +1,3 @@
+require '..\..\..\..\..\ext\packages\ext-theme-base\sass\utils.rb'
+Compass.add_project_configuration('..\..\..\..\..\sass\config.rb')
+cache_path = 'C:\Working\SSOExtJSApp/build/.sass-cache'
---
+++ SSOExtJSApp/build/temp/testing/SSOExtJSApp/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require C:\Working\SSOExtJSApp\app.js
